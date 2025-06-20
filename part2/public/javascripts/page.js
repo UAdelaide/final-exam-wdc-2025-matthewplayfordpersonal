cd4af984b1f5326ee257aeaa09c07d82c5682a3a
matthewplayford <admin@nuts.gg>
2025-06-20 16:56:00 +0000

    [automatic commit] 2025-06-20 16:56:00 UTC 27.33.90.186

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -187,7 +187,7 @@ function login(){
     // Define function to run on response
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            if (this.responseText)
+            if (this.response.data)
             alert("Welcome "+this.responseText);
         } else if (this.readyState == 4 && this.status >= 400) {
             alert("Login failed");
