0dd8dc2d0bfd4d0a81a6e8529c105be1920265d0
matthewplayford <admin@nuts.gg>
2025-06-20 06:05:34 +0000

    [automatic commit] 2025-06-20 06:05:34 UTC 27.33.90.182

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -32,7 +32,7 @@ let db;
 
 router.get('/api/dogs', async function(req, res, next) {
   try {
-    const [rows] = await db.execute('SELECT Dogs.')
+    const [rows] = await db.execute('SELECT Dogs.name AS dog_name')
   } catch (error) {
     res.sendStatus(500).json({ error: 'an error occurred' });
   }
