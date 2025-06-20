9ecfbc0f24d8fe28e5ddf299225c934beeeb5183
matthewplayford <admin@nuts.gg>
2025-06-20 07:18:36 +0000

    [automatic commit] 2025-06-20 07:18:36 UTC 27.33.90.182

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -40,7 +40,7 @@ router.get('/api/open', async function(req, res, next) {
 
 router.get('/api/summary', function(req, res, next) {
   try {
-    const [rows] = await db.execute(`SELECT Users.username as walker_username, Count(WalkApplications.application_id), AVG(WalkRatings.)`)
+    const [rows] = await db.execute(`SELECT Users.username as walker_username, Count(WalkApplications.application_id), AVG(WalkRatings.rating), Count(WalkApplications.)`)
   } catch (error) {
     console.log(error);
     res.json({ error: 'an error occurred' });
