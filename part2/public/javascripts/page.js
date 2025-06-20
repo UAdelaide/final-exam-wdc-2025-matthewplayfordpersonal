7052cdbcf8a2a778f369d919d246a3d3bc4f0f61
matthewplayford <admin@nuts.gg>
2025-06-20 16:56:21 +0000

    [automatic commit] 2025-06-20 16:56:21 UTC 27.33.90.186

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -174,31 +174,14 @@ function downvote(index) {
 }
 
 
-function login(){
+async function login(){a
 
     let user = {
         user: document.getElementById('username').value,
         pass: document.getElementById('password').value
     };
 
-    // Create AJAX Request
-    var xmlhttp = new XMLHttpRequest();
-
-    // Define function to run on response
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            if (this.response.data)
-            alert("Welcome "+this.responseText);
-        } else if (this.readyState == 4 && this.status >= 400) {
-            alert("Login failed");
-        }
-    };
-
-    // Open connection to server & send the post data using a POST request
-    // We will cover POST requests in more detail in week 8
-    xmlhttp.open("POST", "/users/login", true);
-    xmlhttp.setRequestHeader("Content-type", "application/json");
-    xmlhttp.send(JSON.stringify(user));
+    const res = fetch
 
 }
 
