7c98aeea6a2e880cd5746be359c6ec99e2989cfc
matthewplayford <admin@nuts.gg>
2025-06-20 05:59:36 +0000

    [automatic commit] 2025-06-20 05:59:36 UTC 27.33.90.182

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -4,7 +4,31 @@ var mysql = require('mysql2/promise');
 
 let db;
 
+(async () => {
+  try {
+    // Connect to MySQL without specifying a database
+    const connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '' // Set your MySQL root password
+    });
 
+    // Create the database if it doesn't exist
+    await connection.query('CREATE DATABASE IF NOT EXISTS part1');
+    await connection.end();
+
+    // Now connect to the created database
+    db = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'part1'
+    });
+
+  } catch (err) {
+    console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
+  }
+})();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
