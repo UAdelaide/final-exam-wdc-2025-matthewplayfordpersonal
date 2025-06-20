4012a1f477ebcdb05e688267588d6a250d56bd1d
matthewplayford <admin@nuts.gg>
2025-06-20 06:13:28 +0000

    [automatic commit] 2025-06-20 06:13:28 UTC 27.33.90.182

--- part1/app.js
+++ part1/app.js
@@ -48,12 +48,16 @@ let db;
       password: '' // Set your MySQL root password
     });
 
+    // Create the database if it doesn't exist
+    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalk');
+    await connection.end();
+
     // Now connect to the created database
     db = await mysql.createConnection({
       host: 'localhost',
       user: 'root',
       password: '',
-      database: 'DogWalkService'
+      database: 'part1'
     });
 
     // Create a table if it doesn't exist
