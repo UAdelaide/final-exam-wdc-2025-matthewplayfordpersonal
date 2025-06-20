9fbefc11c13f53a026852a8c0d2fb99b88f771a5
matthewplayford <admin@nuts.gg>
2025-06-20 06:29:18 +0000

    [automatic commit] 2025-06-20 06:29:18 UTC 27.33.90.182

--- part1/app.js
+++ part1/app.js
@@ -127,7 +127,7 @@ CREATE TABLE WalkRatings (
 );
             `)
 
-            // this was so anno
+            // this was so annoying
     const arr = [
 `INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner')`,
 `INSERT INTO Users (username, email, password_hash, role) VALUES ('bobwalker', 'bob@example.com', 'hashed456', 'walker')`,
