37ef8e161399ad31217f71c8f1c2f02121077680
matthewplayford <admin@nuts.gg>
2025-06-20 06:29:39 +0000

    [automatic commit] 2025-06-20 06:29:38 UTC 27.33.90.186

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -6,18 +6,6 @@ let db;
 
 (async () => {
   try {
-    // Connect to MySQL without specifying a database
-    const connection = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '' // Set your MySQL root password
-    });
-
-    // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS part1');
-    await connection.end();
-
-    // Now connect to the created database
     db = await mysql.createConnection({
       host: 'localhost',
       user: 'root',
