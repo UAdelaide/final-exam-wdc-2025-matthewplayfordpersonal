fa2cd008e2faf61f9816738720b7460a9bcac893
matthewplayford <admin@nuts.gg>
2025-06-20 05:33:24 +0000

    [automatic commit] 2025-06-20 05:33:24 UTC 27.33.90.186

--- part1/q5.sql
+++ part1/q5.sql
@@ -5,3 +5,4 @@ INSERT INTO Users (username, email, password_hash, role) VALUES ('jeffjeff', 'je
 INSERT INTO Users (username, email, password_hash, role) VALUES ('notjefforbob', 'jeff323@example.com', 'hello', 'walker');
 
 INSERT INTO Dogs (owner_id, name, size) SELECT user_id, 'Max', 'medium' FROM Users WHERE username = 'alice123';
+INSERT INTO Dogs (owner_id, name, size) SELECT user_id, 'Max', 'medium' FROM Users WHERE username = 'alice123';
