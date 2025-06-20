acc48915809b782982ed804bf226db6712e0b437
matthewplayford <admin@nuts.gg>
2025-06-20 17:17:40 +0000

    [automatic commit] 2025-06-20 17:17:40 UTC 27.33.90.182

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -191,6 +191,7 @@ async function login(){
         })
     });
 
+    // redirect to the respective 
     if (res.ok) {
         const data = await res.json();
         if (data.user.role === 'owner') {
