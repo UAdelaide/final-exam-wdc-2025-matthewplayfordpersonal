f6b42eae365cf887526db51c7f665de65ac19a03
matthewplayford <admin@nuts.gg>
2025-06-20 05:25:52 +0000

    [automatic commit] 2025-06-20 05:25:52 UTC 27.33.90.182

--- part1/q5.sql
+++ part1/q5.sql
@@ -1,5 +1,5 @@
 INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner');
-INSERT INTO Users (username, email, password_hash, role) VALUES ('bobwalker', 'alice@example.com', 'hashed123', 'owner');
+INSERT INTO Users (username, email, password_hash, role) VALUES ('bobwalker', 'bob@example.com', 'hashed456', 'owner');
 INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner');
 INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner');
 INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner');
