4ae0d070a99b703f3008a538adf6703c164e9440
matthewplayford <admin@nuts.gg>
2025-06-20 17:14:05 +0000

    [automatic commit] 2025-06-20 17:14:05 UTC 27.33.90.182

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -181,7 +181,8 @@ async function login(){
     const res = await fetch('/api/users/login', {
         method: 'POST',
         body: {
-            user
+            username,
+            password
         }
     });
 
