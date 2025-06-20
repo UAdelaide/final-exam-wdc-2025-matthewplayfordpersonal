636684e4b9e891efb3b4de5e934b3a82af04a450
matthewplayford <admin@nuts.gg>
2025-06-20 06:00:39 +0000

    [automatic commit] 2025-06-20 06:00:39 UTC 27.33.90.186

--- part1/app.js
+++ part1/app.js
@@ -120,7 +120,7 @@ CREATE TABLE WalkRatings (
     `);
 
     // Insert data if table is empty
-    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
+    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM ');
     if (rows[0].count === 0) {
       await db.execute(`
 INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner');
