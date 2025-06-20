76a3db39711f2747d6609fcf7163927e19bf0840
matthewplayford <admin@nuts.gg>
2025-06-20 05:54:06 +0000

    [automatic commit] 2025-06-20 05:54:06 UTC 27.33.90.186

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -10,7 +10,7 @@ router.get('/api/dogs', function(req, res, next) {
   try {
 
   } catch (error) {
-    res.sendStatus(500).json({ error: 'an error occurded' });
+    res.sendStatus(500).json({ error: 'an error occured' });
   }
 });
 
@@ -19,6 +19,7 @@ router.get('/api/open', function(req, res, next) {
 
   } catch (error) {
     res.sendStatus(500);
+    res.sendStatus(500).json({ error: 'an error occured' });
   }
 });
 
